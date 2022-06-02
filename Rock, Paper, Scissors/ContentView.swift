59c5e83fe55f88c722ec27d0279e2d0d57458cf2
NT <usedforfunny@gmail.com>
2022-06-03 00:03:02 +0200

    Initial Commit

---
+++ Rock, Paper, Scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rock, Paper, Scissors
+//
+//  Created by Alexandre Talatinian on 03/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock, Paper, Scissors/Rock__Paper__ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock__Paper__ScissorsApp.swift
+//  Rock, Paper, Scissors
+//
+//  Created by Alexandre Talatinian on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Rock__Paper__ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
